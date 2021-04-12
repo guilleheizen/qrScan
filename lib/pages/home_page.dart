2b74796ce5a97be9dafb91a78e633ff97c1547cf
Mega <guilleheizen@gmail.com>
2021-04-12 15:03:41 -0400

    QR Scanner

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:qrscan/pages/direcciones_page.dart';
+import 'package:qrscan/pages/mapas_page.dart';
+import 'package:qrscan/providers/ui_provider.dart';
+import 'package:qrscan/widgets/custom_navigationbar.dart';
+import 'package:qrscan/widgets/scan_button.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: Text('Historial'),
+        actions: [
+          IconButton(icon: Icon(Icons.delete_forever), onPressed: (){}),
+        ],
+      ),
+      body: _HomePageBody(),
+      bottomNavigationBar: CustomNavigationBar(),
+      floatingActionButton: ScanButton(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
+  }
+}
+
+class _HomePageBody extends StatelessWidget {
+  const _HomePageBody({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    final uiProvider = Provider.of<UiProvider>(context);
+    final currentIndex = uiProvider.selectedMenuOpt;
+    
+    switch(currentIndex){
+      case 0:
+        return MapasPage();
+      case 1:
+        return DireccionesPage();
+      default:
+        return MapasPage();
+    }
+  }
+}
